737e99abc5f2cd1ab9b76af7d7d81618fc14ff50
michal.a <mj_adamski@wp.pl>
2018-07-14 16:15:54 +0200

    java 2

--- src/main/java/j_19/Demo.java
+++ src/main/java/j_19/Demo.java
@@ -3,7 +3,7 @@ package j_19;
 public class Demo {
     public static void main(String[] args) {
 
-
+        
 
     }
 }
--- src/main/java/j_19/Multiplication.java
+++ src/main/java/j_19/Multiplication.java
@@ -4,15 +4,25 @@ public class Multiplication {
 
         void generateMultiplitication(int x, int y) {
 
+            int [][] tab = new int [x+1][y+1];
+
             for (int i = 1; i <= x ; i++) {
 
                 for (int ii = 1; ii<=y; ii++) {
 
-                    System.out.print(i * ii);
+                    //System.out.print(i * ii + "\t");
+                    tab[i][ii] = i * ii;
+                }
+                //System.out.println();
+            }
+
+            for (int i = 1; i < tab.length; i++) {
+
+                for (int j = 1; j < tab[i].length; j++) {
+                    System.out.print(tab[i][j] + "\t");
                 }
                 System.out.println();
             }
 
         }
-
 }
